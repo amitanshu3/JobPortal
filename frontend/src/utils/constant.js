0d84c136083a8dcdb31fb3e701c9ebb71fb6c101
Amitanshu Behura <144086900+amitanshu3@users.noreply.github.com>
2024-09-07 23:42:10 +0530

    Update constant.js

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,6 +1,6 @@
-export const USER_API_END_POINT="http://localhost:8000/api/v1/user";
-export const JOB_API_END_POINT="http://localhost:8000/api/v1/job";
-export const APPLICATION_API_END_POINT="http://localhost:8000/api/v1/application";
-export const COMPANY_API_END_POINT="http://localhost:8000/api/v1/company";
-export const MENTOR_API_END_POINT="http://localhost:8000/api/v1/mentor";
-export const PAYMENT_URL="http://localhost:8000/payment"
+export const USER_API_END_POINT = "https://jobportalbackend-t0hz.onrender.com/api/v1/user";
+export const JOB_API_END_POINT = "https://jobportalbackend-t0hz.onrender.com/api/v1/job";
+export const APPLICATION_API_END_POINT = "https://jobportalbackend-t0hz.onrender.com/api/v1/application";
+export const COMPANY_API_END_POINT = "https://jobportalbackend-t0hz.onrender.com/api/v1/company";
+export const MENTOR_API_END_POINT = "https://jobportalbackend-t0hz.onrender.com/api/v1/mentor";
+export const PAYMENT_URL = "https://jobportalbackend-t0hz.onrender.com/payment";
