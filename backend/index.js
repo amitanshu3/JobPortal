12999d1abc343edb088863a03c139cd273540a97
Amitanshu Behura <144086900+amitanshu3@users.noreply.github.com>
2024-09-07 23:47:22 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -28,7 +28,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 
 const corsOptions = {
-    origin: 'http://localhost:5173',
+    origin: 'https://jobportal-vn4u.onrender.com/',
     
     credentials: true, 
   };
