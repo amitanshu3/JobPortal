62ed4bc2ac22434dc618c3f8b4521a0535cccc71
Amitanshu Behura <144086900+amitanshu3@users.noreply.github.com>
2024-09-20 21:47:01 +0530

    Update Browse.jsx

--- frontend/src/components/Browse.jsx
+++ frontend/src/components/Browse.jsx
@@ -21,7 +21,7 @@ const Browse = () => {
             <Navbar />
             <div className='max-w-7xl mx-auto my-10'>
                 <h1 className='font-bold text-xl my-10'>Search Results ({allJobs.length})</h1>
-                <div className='grid grid-cols-3 gap-4'>
+                <div className='grid grid-cols-1 md:grid-cols-3 gap-4'>
                     {
                         allJobs.map((job) => {
                             return (
