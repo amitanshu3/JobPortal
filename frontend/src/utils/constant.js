fb00cd13dd0c3b097bc264d9ea563698f6e762d0
Amitanshu Behura <144086900+amitanshu3@users.noreply.github.com>
2024-09-24 11:41:44 +0530

    Update constant.js

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,6 +1,6 @@
-export const USER_API_END_POINT = "https://jobportalbackend-t0hz.onrender.com/api/v1/user";
-export const JOB_API_END_POINT = "https://jobportalbackend-t0hz.onrender.com/api/v1/job";
-export const APPLICATION_API_END_POINT = "https://jobportalbackend-t0hz.onrender.com/api/v1/application";
-export const COMPANY_API_END_POINT = "https://jobportalbackend-t0hz.onrender.com/api/v1/company";
-export const MENTOR_API_END_POINT = "https://jobportalbackend-t0hz.onrender.com/api/v1/mentor";
-export const PAYMENT_URL = "https://jobportalbackend-t0hz.onrender.com/payment";
+export const USER_API_END_POINT = "https://jobportal-backend-rg3y.onrender.com/api/v1/user";
+export const JOB_API_END_POINT = "https://jobportal-backend-rg3y.onrender.com/api/v1/job";
+export const APPLICATION_API_END_POINT = "https://jobportal-backend-rg3y.onrender.com/api/v1/application";
+export const COMPANY_API_END_POINT = "https://jobportal-backend-rg3y.onrender.com/api/v1/company";
+export const MENTOR_API_END_POINT = "https://jobportal-backend-rg3y.onrender.com/api/v1/mentor";
+export const PAYMENT_URL = "https://jobportal-backend-rg3y.onrender.com/payment";
