1c6fe8dc1d62cc81e6d217f680912cc5979e5fd9
Amitanshu Behura <144086900+amitanshu3@users.noreply.github.com>
2024-09-28 22:56:11 +0530

    Update HeroSection.jsx

--- frontend/src/components/HeroSection.jsx
+++ frontend/src/components/HeroSection.jsx
@@ -21,7 +21,7 @@ const HeroSection = () => {
                 <span className=' mx-auto px-4 py-2 rounded-full bg-gray-100 text-[#F83002] font-medium'>No. 1 Job Hunt Website</span>
                 <h1 className='text-5xl font-bold'>Search, Apply & <br /> Get Your <span className='text-[#6A38C2]'>Dream Jobs</span></h1>
                 <p>Discover endless opportunities tailored to your skills and aspirations. Take the next step in your career journey today!</p>
-                <div className='flex w-[40%] shadow-lg border border-gray-200 pl-3 rounded-full items-center gap-4 mx-auto'>
+                <div className='flex  max-sm:w-[70%]   w-[40%] shadow-lg border border-gray-200 pl-3 rounded-full items-center gap-4 mx-auto'>
                     <input
                         type="text" 
                         placeholder='Find your dream jobs'
