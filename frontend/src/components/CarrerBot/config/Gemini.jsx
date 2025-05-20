5615f3a97cc4b1fd57402429f13b96557b95cf9d
Amitanshu Behura <144086900+amitanshu3@users.noreply.github.com>
2025-05-20 22:16:24 +0530

    Update Gemini.jsx

--- frontend/src/components/CarrerBot/config/Gemini.jsx
+++ frontend/src/components/CarrerBot/config/Gemini.jsx
@@ -16,7 +16,7 @@ import {
 //   const apiKey = process.env.VITE_APP_ID;
 //   const genAI = new GoogleGenerativeAI(apiKey);
 
-  const genAI = new GoogleGenerativeAI(import.meta.env.GEMINI_API_KEY);
+  const genAI = new GoogleGenerativeAI(import.meta.env.VITE_GEMINI_API_KEY);
   
   const model = genAI.getGenerativeModel({
     model: "gemini-1.5-flash",
